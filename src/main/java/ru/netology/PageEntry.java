95191675ad0fdc5d3676568c181c084cdcb01b63
Nikita Platonov <nplatonov458@gmail.com>
2025-01-11 16:40:19 +0400

    Добавлен конструктор в PageEntry

--- src/main/java/ru/netology/PageEntry.java
+++ src/main/java/ru/netology/PageEntry.java
@@ -1,12 +1,42 @@
 package ru.netology;
 
 public class PageEntry implements Comparable<PageEntry>{
-    private final String pdfName;
-    private final int page;
-    private final int count;
+    private String pdfName;
+    private int page;
+    private int count;
+
+    public PageEntry (String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.count = count;
+        this.page = page;
+    }
+
+    public void setPdfName(String pdfName) {
+        this.pdfName = pdfName;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getPdfName() {
+        return pdfName;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getPage() {
+        return page;
+    }
 
     @Override
     public int compareTo(PageEntry o) {
-        return 0;
+        return Integer.compare(o.getCount(), count);
     }
 }
