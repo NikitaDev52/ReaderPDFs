2e71965762d9c51a3ed83046ab88a76ef6d204af
Nikita Platonov <nplatonov458@gmail.com>
2025-01-11 16:19:06 +0400

    Созданы геттеры, сеттеры для полей. Переопределен метод compareTo

--- src/main/java/ru/netology/BooleanSearchEngine.java
+++ src/main/java/ru/netology/BooleanSearchEngine.java
@@ -1,16 +1,41 @@
 package ru.netology;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BooleanSearchEngine implements SearchEngine{
+    private Map<String, PageEntry> wordsFromPdf = new HashMap<>();
 
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        if (pdfsDir.exists() && pdfsDir.isDirectory() && pdfsDir.length() > 0) {
+            File[] pdsf = pdfsDir.listFiles();
+            for (File pdf : pdsf) {
+                PdfDocument doc = new PdfDocument(new PdfReader(pdf));
+                for (int i = 0; i < doc.getNumberOfPages(); i++) {
+                    PdfPage page = doc.getPage(i);
+                    String text = PdfTextExtractor.getTextFromPage(page);
+                    String[] words = text.split("\\P{IsAlphabetic}+");
+                    Map<String, Integer> freqs = new HashMap<>();
+                    for (var word : words) {
+                        if (word.isEmpty()) {
+                            continue;
+                        }
+                        word = word.toLowerCase();
+                        freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                    }
 
+                }
+            }
+        }
     }
 
 
