5ce776b70badb4e8c52ed60b7f985a42cb622a89
Nikita Platonov <nplatonov458@gmail.com>
2025-01-11 16:47:25 +0400

    Реализован метод BooleanSearchEngine

--- src/main/java/ru/netology/BooleanSearchEngine.java
+++ src/main/java/ru/netology/BooleanSearchEngine.java
@@ -7,20 +7,20 @@ import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class BooleanSearchEngine implements SearchEngine{
-    private Map<String, PageEntry> wordsFromPdf = new HashMap<>();
-
+    private Map<String, List<PageEntry>> wordsFromPdf = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
         if (pdfsDir.exists() && pdfsDir.isDirectory() && pdfsDir.length() > 0) {
             File[] pdsf = pdfsDir.listFiles();
             for (File pdf : pdsf) {
                 PdfDocument doc = new PdfDocument(new PdfReader(pdf));
-                for (int i = 0; i < doc.getNumberOfPages(); i++) {
+                for (int i = 1; i <= doc.getNumberOfPages(); i++) {
                     PdfPage page = doc.getPage(i);
                     String text = PdfTextExtractor.getTextFromPage(page);
                     String[] words = text.split("\\P{IsAlphabetic}+");
@@ -32,7 +32,13 @@ public class BooleanSearchEngine implements SearchEngine{
                         word = word.toLowerCase();
                         freqs.put(word, freqs.getOrDefault(word, 0) + 1);
                     }
+                    for (Map.Entry<String, Integer> entry : freqs.entrySet()) {
+                        String word = entry.getKey();
+                        int count = entry.getValue();
+                        PageEntry pageEntry = new PageEntry(pdf.getName(), i, count);
 
+                        wordsFromPdf.computeIfAbsent(word, k -> new ArrayList<>()).add(pageEntry);
+                    }
                 }
             }
         }
